7beb80754206c80c985e1a70c71e8bdd4bbcafe1
w1ckt0r <w1ckt0rvalim@gmail.com>
2020-10-07 15:55:05 -0300

    historico de lives

--- historico-lives.php
+++ historico-lives.php
@@ -17,25 +17,26 @@ $video_list = json_decode(file_get_contents('https://www.googleapis.com/youtube/
         foreach($video_list->items as $item)
         {
           ?>
+          
 
-            <div class="card col-12 col-md-6 col-lg-4">
-                <div class="card-wrapper">
+            <div class="card col-12 col-md-6 col-lg-4" style="padding-top:2% ;">
+                <div class="card-wrapper" id="<?=$item->id->videoId ?>">
                     <div class="card-img">
                         <img src="<?=$item->snippet->thumbnails->medium->url ?>" alt="Mobirise" title="" class="">
                     </div>
                     <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">Sefaz/SP realiza apresentação do Projeto SAT-RPE</h4>
+                        <h4 class="card-title mbr-fonts-style display-7" style="padding-top: 5%"> <?=$item->snippet->title ?> </h4>
                         <p class="mbr-text mbr-fonts-style display-7">
                             No último dia 17 de novembro a Secretaria da Fazenda de São Paulo apresentou a entidades e empresas os detalhes do projeto SAT-RPE. Na referida reunião, a Sefaz/SP expôs sugestão de cronograma de implantação dos equipamentos que permitirão a medição da vazão das bombas de combustível, como medida ao combate…
                         </p>
                     </div>
-                    <div class="mbr-section-btn text-center"><a href="https://mobirise.co" class="btn btn-primary display-4">
+                    <div class="mbr-section-btn text-center"><a href="<?=$item->id->videoId ?>" class="btn btn-primary display-4">
                             Ler Mais</a></div>
                 </div>
             </div>
+          
 
-
-<?php 
+<?php         
 
         }
 ?>
