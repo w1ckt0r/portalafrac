61bc3486f81839cced35309ba56817275bac0355
w1ckt0r <w1ckt0rvalim@gmail.com>
2020-10-09 15:39:09 -0300

    page-blog

--- blog.php
+++ blog.php
@@ -34,7 +34,7 @@
                         <img src="assets/images/podcastinsta.png" alt="">
                     </div>
                     <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">PIX será ‘tão seguro quanto’ outros sistemas de pagamento, diz diretor do BC</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">PIX será ‘tão seguro quanto’ outros sistemas de pagamento, diz diretor do BC</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             João Manoel Pinho de Mello, diretor do Banco Central, afirmou que o PIX, novo sistema de pagamentos brasileiro, é “um meio de pagamento tão seguro quanto outros meios que já existem” e que os procedimentos de segurança foram feitos do zero, da forma mais
                             moderna que se conhece.
@@ -49,7 +49,7 @@
                         <img src="assets/images/credito-dinheiro.png" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">Peac-Maquininhas: Está liberada a primeira parcela de R$ 5 bilhões para os pequenos empresários</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Peac-Maquininhas: Está liberada a primeira parcela de R$ 5 bilhões para os pequenos empresários</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             Uma boa notícia para microempreendedores individuais (MEI): está liberada a primeira parcela do Programa Emergencial de Acesso ao Crédito (PEAC), voltada às micro e pequenas empresas que faturam até R$ 4,8 milhões por ano. O valor é de R$ 5 bilhões para
                             a modalidade de garantia de recebíveis (Peac-Maquininhas), que pretende oferecer R$ 10 bilhões de recursos da União.
@@ -63,7 +63,7 @@
                         <img src="assets/images/nfc-e-saiba-o-que-e.jpg" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">Publicado o Decreto 864/2020 em Santa Catarina que faculta ao contribuinte a possibilidade de emissão de NFC-e.</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Publicado o Decreto 864/2020 em Santa Catarina que faculta ao contribuinte a possibilidade de emissão de NFC-e.</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             Desta forma, abre-se a possibilidade de adesão a este documento fiscal, sem mais a necessidade de piloto.
                         </p>
@@ -76,7 +76,7 @@
                         <img src="assets/images/como-os-bancos-estao-se-preparando-para-oferecer-o-pix-aos-seus-clientes.webp" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">Como os bancos estão se preparando para oferecer o PIX aos seus clientes?</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Como os bancos estão se preparando para oferecer o PIX aos seus clientes?</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             Com lançamento previsto para novembro deste ano, o PIX tem sido pauta frequente no noticiário econômico e vem despertando a curiosidade da população. Já falamos bastante sobre esse tema, mas a menos de dois meses do lançamento do produto no mercado, é
                             interessante entender como as instituições financeiras estão se preparando para oferecer essa ferramenta.
@@ -96,7 +96,7 @@
                         <img src="assets/images/tributario.png" alt="">
                     </div>
                     <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">Bolsonaro sanciona lei que muda regras de cobrança de ISS</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Bolsonaro sanciona lei que muda regras de cobrança de ISS</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             A nova lei prevê uma transição para a partilha do produto da arrecadação do tributo, do município do local do estabelecimento prestador para o de domicílio do tomador dos serviços. Ou seja, transfere a competência de cobrança do imposto para o município
                             onde o serviço é prestado ao usuário final.
@@ -111,7 +111,7 @@
                         <img src="assets/images/whatsapp-pay.png" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">O que se sabe até agora sobre o WhatsApp Pagamentos?</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">O que se sabe até agora sobre o WhatsApp Pagamentos?</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             Entre as várias carteiras digitais existentes hoje, uma ainda em fase de testes desperta a curiosidade nas pessoas: o WhatsApp Pagamentos. O que se sabe até agora sobre o WhatsApp Pagamentos? Leia mais neste artigo.
                         </p>
@@ -124,7 +124,7 @@
                         <img src="assets/images/podcastinsta.png" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">Banco Central Divulga a API Pix</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Banco Central Divulga a API Pix</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             O Banco Central do Brasil publicou, em complemento ao disposto no Informe Pix – 043/2020, a API Pix, uma API padronizada que tem como objetivo principal facilitar o processo de integração com soluções de automação, ampliar a concorrência no setor e possibilitar
                             menores custos aos usuários finais.
@@ -138,7 +138,7 @@
                         <img src="assets/images/podcastinsta.png" alt="">
                     </div>
                     <div class="card-box ">
-                        <h4 class="card-title mbr-fonts-style display-7">Confira o 1º episódio do podcast “Ícones da Automação” produzido pela AFRAC.</h4>
+                        <a href="#"><h4 class="card-title mbr-fonts-style display-7">Confira o 1º episódio do podcast “Ícones da Automação” produzido pela AFRAC.</h4></a>
                         <p class="mbr-text mbr-fonts-style display-7">
                             Com o propósito de gerarmos mais conteúdo de valor ao nosso associado, foi criado o podcast “Ícones da Automação” que trará em cada episódio um grande nome do segmento de tecnologia para o comércio e serviços. Confira o link e siga nosso canal no Spotify!
                         </p>
---
+++ page-blog.php
@@ -0,0 +1,37 @@
+<?php require("header.php"); ?>
+
+<section class="counters5 counters cid-scWFbmtght" id="counters5-6">
+
+    <div class="container pt-4 mt-2">
+        <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2">Why Choose Us</h2>
+        <h3 class="mbr-section-subtitle pb-5 align-center mbr-fonts-style display-5">
+            Responsive block with counters and media.
+        </h3>
+        <div class="row justify-content-center">
+            <div class="cards-block">
+                <div class="cards-container">
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex">
+                            <div class="card-img pr-3">
+                                <h3 class="img-text d-flex align-items-center justify-content-center">
+                                    1
+                                </h3>
+                            </div>
+                            <div class="card-text">
+                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Honest and Dependable</h4>
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                    sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd </p>              
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php require("footer.php"); ?>
