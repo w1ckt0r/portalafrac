9bb2a54b4d4f6ad07ac175ddc2715dfd9f229f88
w1ckt0r <w1ckt0rvalim@gmail.com>
2020-10-15 14:34:58 -0300

    index

--- page-blog.php
+++ page-blog.php
@@ -1,37 +1,39 @@
 <?php require("header.php"); ?>
 
-<section class="counters5 counters cid-scWFbmtght" id="counters5-6">
 
-    <div class="container pt-4 mt-2">
-        <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2">Why Choose Us</h2>
-        <h3 class="mbr-section-subtitle pb-5 align-center mbr-fonts-style display-5">
-            Responsive block with counters and media.
-        </h3>
-        <div class="row justify-content-center">
-            <div class="cards-block">
-                <div class="cards-container">
-                    <div class="card px-3 align-left col-12">
-                        <div class="panel-item p-4 d-flex">
-                            <div class="card-img pr-3">
-                                <h3 class="img-text d-flex align-items-center justify-content-center">
-                                    1
-                                </h3>
-                            </div>
-                            <div class="card-text">
-                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Honest and Dependable</h4>
-                                <p class="mbr-content-text mbr-fonts-style display-7">
-                                    sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd </p>              
-                            </div>
-                        </div>
-                    </div>
+<section class="mbr-section content4 cid-sbUhHqwSeI" id="content4-1w">
 
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8" style="padding: 5% 0% 2% 0%">
+                <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                    <strong> PIX será ‘tão seguro quanto’ outros sistemas de pagamento, diz diretor do BC</strong>
+                </h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5"></h3>
 
-
-                </div>
             </div>
         </div>
     </div>
-
 </section>
+
+<section class="mbr-section article content1 cid-scWNzBraAR" id="content1-7">
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-text col-12 mbr-fonts-style display-5 col-md-12">
+                <div class="align-center display-4" style="padding-bottom: 5%;">
+                    <p>
+                        Antes mesmo do funcionamento do sistema, golpistas usaram o cadastro da Chave PIX para roubo de dados. Instituições financeiras poderão reter transferências por até uma hora em caso de suspeita de irregularidade.
+                    </p>
+
+                </div>
+                <p>
+                    Mobirisessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. Mobirise Website Builder creates responsive, retina and <em>mobile friendly websites</em> in a few clicks. Mobirise is one of the easiest website development tools available today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php require("footer.php"); ?>
