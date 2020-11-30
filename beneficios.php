a7139dc6fd8faf98572e90826d031da4dd709551
w1ckt0r <w1ckt0rvalim@gmail.com>
2020-11-30 16:31:24 -0300

    beneficios

--- beneficios.php
+++ beneficios.php
@@ -61,8 +61,9 @@
                                     Aproximação e networking com os provedores de automação para o comércio
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt ">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo.</p>
+                                    Através da participação em reuniões, eventos e encontros promovidos pela Entidade,
+                                    as empresas associadas possuem acessos a vários Players do Setor, gerando oportunidade de parcerias e negócios estratégicos
+                                </p>
                             </div>
                         </div>
 
@@ -80,11 +81,11 @@
                         <div class="media-content">
                             <div class="mbr-section-text">
                                 <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
-                                    Assessoria técnica e normativa
+                                    Grupos Técnicos de Trabalho
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
+                                    Os Grupos Técnicos de Trabalho promovidos pela AFRAC reúnem os diversos especialistas das empresas filiadas, proporcionando um ambiente de troca de informações e elaborações de projetos visando alternativas e impulsionamento de todo o Setor. Através dos Grupos Técnicos de Trabalho, a AFRAC cria, compartilha e colabora com a evolução das tecnologias aplicadas ao varejo.
+                                </p>
                             </div>
                         </div>
                         <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
@@ -110,8 +111,9 @@
                                     Assessoria Jurídica
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo.</p>
+                                    Corpo jurídico especializado em tecnologias aplicadas ao varejo, prestando consultoria às empresas filiadas e
+                                    apoiando o Setor quanto a elaboração de informativos e materiais atualizados sobre as legislações federais, estaduais e municipais de impacto.
+                                </p>
                             </div>
                         </div>
 
@@ -133,8 +135,8 @@
                                     Defesa de interesses do setor junto aos órgãos e entidades governamentais
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo.</p>
+                                    Canal de comunicação entre as empresas filiadas e os diversos órgãos governamentais, representando e atuando como o porta-voz do Setor na busca de medidas que visem o desenvolvimento da tecnologia.
+                                </p>
                             </div>
                         </div>
                         <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
@@ -160,8 +162,8 @@
                                     Descontos de até 50% em cursos, palestras e eventos – além de descontos exclusivos na AUTOCOM
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
+                                    A AFRAC oferece aos seus associados conteúdos de capacitação e atualização através de cursos elaborados por especialistas em diversas áreas, e que podem ser adquiridos com condições especiais, além de descontos em eventos e palestras com temas relevantes e qualificados.
+                                </p>
                             </div>
                         </div>
 
@@ -183,8 +185,9 @@
                                     Fomento de ações para incentivar o fortalecimento, uso e tendências da Automação para o Comércio
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo.
+                                    Atuando através da organização de visitação em eventos internacionais, promoção de palestras e debates sobre as principais tendências e inovações do Setor.
+
+
                                 </p>
                             </div>
                         </div>
@@ -208,11 +211,11 @@
                         <div class="media-content">
                             <div class="mbr-section-text">
                                 <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left;font-size: 14pt">
-                                    Oferta de módulos de treinamento
+                                    Parceria Tributarie
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
+                                    Parceria que proporciona condições especiais as empresas associadas e que busca dar o suporte adequado para o 
+                                    planejamento tributário além de análise assertiva sobre a viabilidade de recuperação de impostos </p>
                             </div>
                         </div>
 
@@ -231,11 +234,11 @@
                         <div class="media-content">
                             <div class="mbr-section-text">
                                 <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
-                                    Palestras informativas
+                                Salas de reuniões disponíveis para uso do associado 
                                 </h5>
                                 <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right;font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
+                                A AFRAC oferece em sua estrutura física uma sala de reuniões que pode ser utilizada por seus associados mediante agendamento prévio (Prezando pela saúde e segurança dos associados, suspendemos esse recurso por período indeterminado, devido ao atual cenário causado pela pandemia).
+                             </p>
                             </div>
                         </div>
                         <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
@@ -245,57 +248,7 @@
                 </div>
             </div>
         </div>
-    </section>
-    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
 
-        <div class="container">
-            <div class="media-container-row">
-                <div class="col-12 col-md-10">
-                    <div class="media-container-row">
-                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
-                            <img src="assets/images/beneficios/3-beneficios.png" alt="Mobirise">
-                        </div>
-                        <div class="media-content">
-                            <div class="mbr-section-text">
-                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left;font-size: 14pt">
-                                    Suporte para o planejamento tributário da sua empresa através da Tributarie, com desconto na tabela de serviços.
-                                </h5>
-                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
-
-        <div class="container">
-            <div class="media-container-row">
-                <div class="col-12 col-md-10">
-                    <div class="media-container-row">
-
-                        <div class="media-content">
-                            <div class="mbr-section-text">
-                                <h5 class="card-title pb-3 mbr-bold mbr-fonts-style display-5" style="text-align: right;font-size: 14pt">
-                                    Salas de reuniões disponíveis para uso do associado
-                                </h5>
-                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
-                                    Aqui entra um subtexto explicativo sobre o tema acima, acreditamos que isso ajudará
-                                    a deixar o layout mais adequado e com conteúdo mais funcional e atrativo. </p>
-                            </div>
-                        </div>
-                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
-                            <img src="assets/images/beneficios/1-beneficios.png" alt="Mobirise">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
 </section>
 
