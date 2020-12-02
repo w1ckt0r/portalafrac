5697e69b64d5b1a853ed0b0ff3b2df9d31d7ae05
w1ckt0r <w1ckt0rvalim@gmail.com>
2020-12-02 20:17:32 -0300

    ecossistema

--- ecossistema.php
+++ ecossistema.php
@@ -11,7 +11,244 @@
     </div>
 </section>
 
-<section>
+<section class="timeline2 cid-s8C16i90OO" id="timeline2-13">
+
+
+
+
+
+    <div class="container align-center">
+
+
+
+        <div class="container timelines-container" mbri-timelines="">
+            <!--1-->
+            <div class="row timeline-element reverse separline">
+
+                <span class="iconsBackground">
+                    <span class="mbri-pages mbr-iconfont"></span>
+                </span>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5"><div></div>
+                        </h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">
+                            Abordamos aspectos técnicos, mercadológicos, regulatórios e jurídicos, interagindo com entidades regulatórias na defesa dos interesses dos associados.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/industry.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--2-->
+            <div class="row timeline-element  separline">
+                <span class="iconsBackground">
+                    <span class="mbri-responsive mbr-iconfont"></span>
+                </span>
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5"></h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">
+                            Atuar fortemente nas iniciativas de softwares para automação comercial. A equipe interna Afrac e os grupos de trabalho de associados analisam as necessidades das software houses e do varejo visando soluções seguras e com inovação tecnológica.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/laptop-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--3-->
+            <div class="row timeline-element reverse separline">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbri-setting"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5"></h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">
+                            Os suprimentos utilizados pelo Setor de Automação mais comuns são os identificadores de produtos e comprovantes de compra. Os suprimentos cumprem a importante missão de traduzir as informações tratadas nos sistemas, como o rastreamento e a identificação de produtos registrados a partir de códigos de barras, QRcodes, ou RFIDs.
+                            Garantia tanto para estabelecimentos quanto para consumidores, os suprimentos complementam a segurança de processos ao fornecer um suporte físico legível de transações automatizadas aos olhos humanos. NÚMEROS DO SEGMENTO No Brasil, o mercado de suprimentos movimenta entre 50 a 55 mil toneladas de papel. O varejo de alimentos, cartões de crédito, bancos, estacionamentos, logística e demais segmentos são os que mais necessitam de controle e emissão de comprovantes em seus processos de automação. Em um mês, cada cidadão brasileiro consome 280 g/m² de cupons, etiquetas, tickets, extratos bancários e comprovantes em geral. COMO O SEGMENTO DE SUPRIMENTOS SE DIVIDE • Bobinas: confeccionadas em papel térmico, finalizam o processo do registro da venda com o Cupom Fiscal e no futuro o Extrato da compra na NFCe/SAT. • Etiquetas Adesivas, Tickets controle de acesso, tags: são utilizados para impressão do Código de Barras/QRCode e demais informações do produto. </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/file-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--4-->
+            <div class="row timeline-element  separline">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbri-delivery"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5"></h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Atuamos em duas frentes: uma institucional, que representa os interesses gerais do setor, junto a órgãos e Entidades governamentais municipais, estaduais e federais; e outra na vertente técnica, que presta suporte e assessoria jurídica aos associados e fomenta o debate legal de interesse do setor na mídia, imprensa e redes sociais corporativas.
+                            <br>
+                            <br></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/barcode-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--5-->
+            <div class="row timeline-element reverse separline">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont socicon-reverbnation socicon"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                            AUTOCOM</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">
+                             AIDC-RFID
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/point-of-service-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--6-->
+            <div class="row timeline-element separline">
+                <span class="iconsBackground">
+                    <span class="mbri-pages mbr-iconfont"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                        </h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Participar e inovar no segmento de soluções para meios de pagamento e sua integração no cenário de automação comercial.<br></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/truck-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--7-->
+            <div class="row timeline-element reverse">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mobi-mbri-extension mobi-mbri"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                            &nbsp;</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">O QUE SÃO OS CANAIS DE DISTRIBUIÇÃO Os canais de comercialização e distribuição são responsáveis por levar a tecnologia do Setor de Automação ao consumidor final de forma que seu uso seja pleno e resulte em benefícios aos negócios de ambos. Os canais cumprem papéis de extrema relevância: <br>
+                        • Analisar, propor e dimensionar soluções no domínio da tecnologia. • Promover a proximidade com o cliente e seu negócio de forma a entender como a tecnologia aplicada pode gerar os melhores resultados. • Implantar, acompanhar e prestar manutenção dos recursos de automação envolvidos na solução de forma a garantir seu pleno funcionamento. NÚMEROS DO SEGMENTO Existem mais de 20 mil revendas de Tecnologia da Informação (TI) e Tecnologia da Automação (TA) no Brasil. Aproximadamente 300 empresas se configuram com as mais representativas e focadas em TA. COMO O SEGMENTO DE CANAIS SE DIVIDE Entre os principais canais, estão os distribuidores, as revendas e os agentes comerciais. O atendimento prestado pelas empresas que compõe os canais agregam Hardware, Software e Serviços
+                    </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/paper-plane-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row timeline-element separline">
+                <span class="iconsBackground">
+                    <span class="mbri-pages mbr-iconfont"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                        </h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7"><br></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/placeholder-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row timeline-element reverse">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mobi-mbri-extension mobi-mbri"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                            &nbsp;</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7"></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/store-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row timeline-element separline">
+                <span class="iconsBackground">
+                    <span class="mbri-pages mbr-iconfont"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                        </h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7"><br></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/certification-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <div class="row timeline-element reverse">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mobi-mbri-extension mobi-mbri"></span>
+                </span>
+
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                            &nbsp;</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7"></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div>
+                        <img src="assets/images/printer-150x150.png" style="width: 450px;">
+                    </div>
+                </div>
+            </div>
+            <!--8-->
+
+            <!--9-->
+
+            <!--10-->
+
+            <!--11-->
+
+            <!--12-->
+
+        </div>
+    </div>
+</section>
+<!-- <section>
 
     <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
 
@@ -286,7 +523,7 @@
         </div>
     </section>
 
-</section>
+</section> -->
 
 <!-- <div class="container">
     <div class="media-container-row pt-5">
