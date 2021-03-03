2d4f26e708173b8ccdfbb1ac16df726357ec7afd
w1ckt0r <w1ckt0rvalim@gmail.com>
2021-03-03 14:24:01 -0300

    universidade

---
+++ universidade.php
@@ -0,0 +1,259 @@
+<?php require("header.php"); ?>
+
+
+
+
+<section class="features2 cid-s8HaGIkBjh" id="features2-3" style="background-color: #04112b;">
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-10 align-center">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-2" style="color: white;">Beneficios</h1>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+<section>
+
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding:10px 2%; background-color:#ececec">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style align-right" style="color: #04112b;padding-top:10%">Oferecemos apoio, defendemos interesses, levantamos oportunidades e aproximamos nossos associados.
+                                    <br> Saiba tudo o que você tem a ganhar ao fazer parte da AFRAC. Aproveite ao máximo cada um dos benefícios
+                                    <br> que trabalhamos para criar e manter.
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                </h5>
+                            </div>
+                            <hr style="border-top: 1px solid #1b477c;">
+                        </div>
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/destaque-beneficios.png" alt="AFRAC">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="mbr-figure " style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/2-beneficios.png" alt="AFRAC">
+                        </div>
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left;font-size: 14pt">
+                                    Aproximação e networking com os provedores de automação para o comércio
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt ">
+                                    Através da participação em reuniões, eventos e encontros promovidos pela Entidade,
+                                    as empresas associadas possuem acessos a vários Players do Setor, gerando oportunidade de parcerias e negócios estratégicos
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
+                                    Grupos Técnicos de Trabalho
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
+                                    Os Grupos Técnicos de Trabalho promovidos pela AFRAC reúnem os diversos especialistas das empresas filiadas, proporcionando um ambiente de troca de informações e elaborações de projetos visando alternativas e impulsionamento de todo o Setor. Através dos Grupos Técnicos de Trabalho, a AFRAC cria, compartilha e colabora com a evolução das tecnologias aplicadas ao varejo.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/3-beneficios.png" alt="AFRAC">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/1-beneficios.png" alt="AFRAC">
+                        </div>
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left; font-size: 14pt">
+                                    Assessoria Jurídica
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
+                                    Corpo jurídico especializado em tecnologias aplicadas ao varejo, prestando consultoria às empresas filiadas e
+                                    apoiando o Setor quanto a elaboração de informativos e materiais atualizados sobre as legislações federais, estaduais e municipais de impacto.
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
+                                    Defesa de interesses do setor junto aos órgãos e entidades governamentais
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
+                                    Canal de comunicação entre as empresas filiadas e os diversos órgãos governamentais, representando e atuando como o porta-voz do Setor na busca de medidas que visem o desenvolvimento da tecnologia.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/2-beneficios.png" alt="AFRAC">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/3-beneficios.png" alt="AFRAC">
+                        </div>
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left;font-size: 14pt">
+                                    Descontos de até 50% em cursos, palestras e eventos – além de descontos exclusivos na AUTOCOM
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
+                                    A AFRAC oferece aos seus associados conteúdos de capacitação e atualização através de cursos elaborados por especialistas em diversas áreas, e que podem ser adquiridos com condições especiais, além de descontos em eventos e palestras com temas relevantes e qualificados.
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
+                                    Fomento de ações para incentivar o fortalecimento, uso e tendências da Automação para o Comércio
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right; font-size: 12pt">
+                                    Atuando através da organização de visitação em eventos internacionais, promoção de palestras e debates sobre as principais tendências e inovações do Setor.
+
+
+                                </p>
+                            </div>
+                        </div>
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/1-beneficios.png" alt="AFRAC">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/2-beneficios.png" alt="AFRAC">
+                        </div>
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: left;font-size: 14pt">
+                                    Parceria Tributarie
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: left; font-size: 12pt">
+                                    Parceria que proporciona condições especiais as empresas associadas e que busca dar o suporte adequado para o 
+                                    planejamento tributário além de análise assertiva sobre a viabilidade de recuperação de impostos </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mbr-section content7 cid-sfTggWxT7q" id="content7-25" style="padding: 2%">
+
+        <div class="container">
+            <div class="media-container-row">
+                <div class="col-12 col-md-10">
+                    <div class="media-container-row">
+
+                        <div class="media-content">
+                            <div class="mbr-section-text">
+                                <h5 class="card-title pb-3 mbr-fonts-style mbr-bold display-5" style="text-align: right;font-size: 14pt">
+                                Salas de reuniões disponíveis para uso do associado 
+                                </h5>
+                                <p class="card-title pb-3 mbr-fonts-style display-6" style="text-align: right;font-size: 12pt">
+                                A AFRAC oferece em sua estrutura física uma sala de reuniões que pode ser utilizada por seus associados mediante agendamento prévio (Prezando pela saúde e segurança dos associados, suspendemos esse recurso por período indeterminado, devido ao atual cenário causado pela pandemia).
+                             </p>
+                            </div>
+                        </div>
+                        <div class="mbr-figure" style="width: 100%;padding-left: 4rem;">
+                            <img src="assets/images/beneficios/2-beneficios.png" alt="AFRAC">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</section>
+
+
+
+
+
+<?php require("footer.php"); ?>
